64d1a2c36f3dbd8d3b6cbaa7fc870805d29a994c
David Jennings <david@chillidoor.com>
2024-10-01 22:03:35 +0100

    adds gem code

--- lib/proxypoke.rb
+++ lib/proxypoke.rb
@@ -1,6 +1,31 @@
+require "net/http"
+
 module ProxyPoke
-  def self.run
-    puts "Poke a proxy"
+  def self.poke(url, proxy_host="localhost", proxy_port=3128)
+    uri = URI(url)
+
+    client = Net::HTTP.new(
+      address = uri.host,
+      port = uri.port,
+      p_addr = proxy_host,
+      p_port = proxy_port,
+    )
+
+    client.use_ssl = uri.scheme == "https" ? true : false
+    client.read_timeout = 3
+    
+    begin
+      response = client.get(url)
+    rescue Net::ReadTimeout
+      puts "Error: connection timed out after #{client.read_timeout} seconds"
+      exit(false)
+    rescue Errno::ECONNREFUSED
+      puts "Error: could not connect to proxy #{proxy_host} on port #{proxy_port}"
+      exit(false)
+    end
+
+    puts "Success: #{proxy_host}:#{proxy_port} is working"
+    exit(true)
   end
 
 
---
+++ proxypoke.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+    s.name                    = "proxypoke"
+    s.version                 = "1.0.0"
+    s.summary                 = "Makes an HTTP request to check if a web proxy is working"
+    s.authors                 = ["David Jennings"]
+    s.email                   = "david@chillidoor.com"
+    s.required_ruby_version   = ">= 2.7"
+    s.files                   = Dir["lib/*.rb"]
+    s.executables             = ["proxypoke"] 
+    s.homepage                = "https://github.com/chillidoor/proxypoke/"
+    s.license                 = "MIT"
+
+  end
