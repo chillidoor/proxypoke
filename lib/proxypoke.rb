5ef495e445623e7bb6f13adf70e0157b8eccf747
David Jennings <david@chillidoor.com>
2024-11-26 19:31:16 +0000

    adds option to search for string in response body
    In addition to adding the new option, I've renamed the stub domain in
    the tests so it's clear that the tests don't actually make HTTP
    requests.

--- lib/proxypoke.rb
+++ lib/proxypoke.rb
@@ -1,7 +1,7 @@
 require "net/http"
 
 module ProxyPoke
-  def self.poke(url, proxy_host, proxy_port, read_timeout)
+  def self.poke(url, proxy_host, proxy_port, read_timeout, search_string)
     uri = URI(url)
 
     client = Net::HTTP.new(
@@ -16,11 +16,21 @@ module ProxyPoke
     
     begin
       response = client.get(url)
-      result, message = "Success", "#{proxy_host}:#{proxy_port} is working"
     rescue Net::ReadTimeout
       result, message = "Error", "connection timed out after #{client.read_timeout} seconds"
     rescue Errno::ECONNREFUSED
       result, message = "Error", "could not connect to proxy #{proxy_host} on port #{proxy_port}"
+    else
+      response_body = response.read_body
+
+      if search_string == ""
+        result, message = "Success", "#{proxy_host}:#{proxy_port} is working"
+      elsif response_body.include? search_string
+        result, message = "Success", "#{proxy_host}:#{proxy_port} is working and the search string was found"
+      else
+        result, message = "Error", "#{proxy_host}:#{proxy_port} is working but the search string was not found"
+      end
+
     end
 
     return result, message
--- tests/test_proxypoke.rb
+++ tests/test_proxypoke.rb
@@ -6,23 +6,37 @@ require "proxypoke"
 
 class ProxyPokeTest < Minitest::Test
   def test_cant_connect_to_proxy
-    stub_request(:get, "https://www.google.com").to_raise(Errno::ECONNREFUSED)
-    result = ProxyPoke.poke("https://www.google.com", "localhost", "3128", 1)
+    stub_request(:get, "https://dummysite").to_raise(Errno::ECONNREFUSED)
+    result = ProxyPoke.poke("https://dummysite", "localhost", "3128", 1)
     
     assert_equal result, ["Error", "could not connect to proxy localhost on port 3128"]
   end
 
   def test_read_timeout
-    stub_request(:get, "https://www.google.com").to_raise(Net::ReadTimeout)
-    result = ProxyPoke.poke("https://www.google.com", "localhost", "3128", 0.1)
+    stub_request(:get, "https://dummysite").to_raise(Net::ReadTimeout)
+    result = ProxyPoke.poke("https://dummysite", "localhost", "3128", 0.1)
 
     assert_equal result, ["Error", "connection timed out after 0.1 seconds"]
   end
 
   def test_successful_request
-    stub_request(:get, "https://www.google.com")
-    result = ProxyPoke.poke("https://www.google.com", "localhost", "3128", 1)
+    stub_request(:get, "https://dummysite")
+    result = ProxyPoke.poke("https://dummysite", "localhost", "3128", 1)
 
     assert_equal result, ["Success", "localhost:3128 is working"]
+  end
+
+  def test_search_string_not_found_in_response
+    stub_request(:get, "https://dummysite").to_return(body: "dummy body")
+    result = ProxyPoke.poke("https://dummysite", "localhost", "3128", 1, "string")
+
+    assert_equal result, ["Error", "localhost:3128 is working but the search string was not found"]
+  end
+
+  def test_search_string_found_in_response
+    stub_request(:get, "https://dummysite").to_return(body: "dummy body")
+    result = ProxyPoke.poke("https://dummysite", "localhost", "3128", 1, "dummy body")
+
+    assert_equal result, ["Success", "localhost:3128 is working and the search string was found"]
   end
 end
